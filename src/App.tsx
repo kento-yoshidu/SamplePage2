9a1fbd04335292a04b2b90c6a85ec1a492b417dc
kento <kento@toriwatari.bird>
2025-04-13 20:36:00 +0900

    ⚡ Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import './App.css'
 import styles from './css/style.module.css';
-import { Page2 } from './components/page2';
-import { Div3 } from './components/div3';
 
 function App() {
   return (
-    <>
-      <div className={styles.grid}>
-        <div className={styles.item1}>
-          item1
-        </div>
+    <div className={`${styles.dashboard}`}>
+      <header className={`${styles.header} ${styles.backgroundWhite} ${styles.borderBottom}`}>
+        header
+      </header>
+      <main className={`${styles.main} ${styles.backgroundWhitesmoke}`}>
+        main
+      </main>
 
-        <div className={styles.item2}>
-          item2
-        </div>
-
-        <div className={styles.item3}>
-          item3
-        </div>
-
-        <div className={styles.item4}>
-          item4
-        </div>
-
-        <div className={styles.item5}>
-          item5
-        </div>
+      <div className={`${styles.side} ${styles.darkBar}`}>
+        side
       </div>
-
-      <Page2 />
-
-      <Div3 />
-    </>
+    </div>
   )
 }
 
---
+++ src/components/div4.tsx
@@ -0,0 +1,28 @@
+import styles from "../css/div4.module.css";
+
+export const Div4 = () => {
+  return (
+    <div className={styles.grid}>
+      <div className={styles.item1}>
+        item1
+      </div>
+
+      <div className={styles.item2}>
+        item2
+      </div>
+
+      <div className={styles.item3}>
+        item3
+      </div>
+
+      <div className={styles.item4}>
+        item4
+      </div>
+
+      <div className={styles.item5}>
+        item5
+      </div>
+    </div>
+  )
+}
+
